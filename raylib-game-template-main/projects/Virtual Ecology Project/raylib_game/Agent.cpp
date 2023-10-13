b586d641b64ae8b391ce75e7f50c2a78d017620a
VivaanBhole <bhole.vivaan8421@gmail.com>
2023-10-13 12:08:32 -0400

    added Agent Constructor

--- raylib-game-template-main/projects/Virtual Ecology Project/raylib_game/Agent.cpp
+++ raylib-game-template-main/projects/Virtual Ecology Project/raylib_game/Agent.cpp
@@ -4,9 +4,20 @@
 class Agent {
 
 public:
+
+    Agent(Vector2 location, float size, float maxSpeed, float maxForce, float foodRange, float detectRange, float rechargeTime) {
+        this->location = location;
+        this->size = size;
+        this->maxSpeed= maxSpeed;
+        this->maxForce = maxForce;
+        this->foodRange = foodRange;
+        this->detectRange = detectRange;
+        this->rechargeTime = rechargeTime;
+    }
+
     Vector2 location;
     Vector2 velocity;
     Vector2 acceleration;
     Vector2 targetPos;
-    float size, maxspeed, maxforce, foodRange, detectRange, rechargeTime;
+    float size, maxSpeed, maxForce, foodRange, detectRange, rechargeTime;
 };
--- raylib-game-template-main/projects/Virtual Ecology Project/raylib_game/ChaserAgent.cpp
+++ raylib-game-template-main/projects/Virtual Ecology Project/raylib_game/ChaserAgent.cpp
@@ -1,26 +1,14 @@
 #include "Header.h"
 #include "Agent.cpp"
 
+const float SIZE = 1, MAX_SPEED = 1, MAX_FORCE = .1, FOOD_RANGE = 1, DETECT_RANGE = 2, RECHARGE_TIME = 0;
 
 class ChaserAgent : public Agent {
 public:
+    
+    ChaserAgent(Vector2 location) : Agent(location, SIZE, MAX_SPEED, MAX_FORCE, FOOD_RANGE, DETECT_RANGE, RECHARGE_TIME) {}
 
 
-    //it seeks
-    void updatePlayerNums() {
-        lookForFood();
-        if (&targetPos == NULL)
-            velocity = Vector2Add(velocity, acceleration);
-        velocity = Vector2ClampValue(velocity, -maxspeed, maxspeed);
-        location = Vector2Add(location, velocity);
-
-        Vector2 desiredVelocity = Vector2Normalize(Vector2Subtract(targetPos, location));
-        addForce(Vector2ClampValue(
-            Vector2Subtract(
-                { desiredVelocity.x * maxspeed, desiredVelocity.y * maxspeed }
-                , velocity)
-            , -maxforce, maxforce));
-    }
 
     void lookForFood() {
 
